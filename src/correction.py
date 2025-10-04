f90ce9dfa3ca47625e1a85fb0b5fb6d048cf5add
FAAlekseev <faalekseev@maieducation.com>
2025-10-04 15:13:06 +0300

    Cut-n-Paste fix

--- src/correction.py
+++ src/correction.py
@@ -8,31 +8,15 @@ def correction(tok):
     :param tok: Отдельное выражение (либо не разделенные токены, либо содержащее лишние символы)
     :return: Правильный для обработки список токенов
     """
-    if '(' in tok:
-        tok = tok.replace('(', ' ( ')
-    if ')' in tok:
-        tok = tok.replace(')', ' ) ')
+    for op in OPERATORS + BRACKETS + UNARY:
+        if op in tok:
+            tok = tok.replace(op, f" {op} ")
+
     if ',' in tok:
         tok = tok.replace(',', '.')
-    if '**' in tok:
-        tok = tok.replace('**', ' ** ')
-    if '~' in tok:
-        tok = tok.replace('~', ' ~ ')
-    if '$' in tok:
-        tok = tok.replace('$', ' $ ')
-    if '*' in tok and '**' not in tok:
-        tok = tok.replace('*', ' * ')
-    if '//' in tok:
-        tok = tok.replace('//', ' // ')
-    if '/' in tok and '//' not in tok:
-        tok = tok.replace('/', ' / ')
-    if '+' in tok:
-        tok = tok.replace('+', ' + ')
-    if '-' in tok:
-        tok = tok.replace('-', ' - ')
-    if '%' in tok:
-        tok = tok.replace('%', ' % ')
+
     tok = tok.split()
+
     for part in tok:
         if not (part in OPERATORS  or part in UNARY or part in BRACKETS or is_float(part)):
             raise SyntaxError(f"Wrong input: {part}")
